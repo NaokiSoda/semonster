cee40bfd56cf9ae073ba9a2d0446648c07db9349
NaokiSoda <n.sou2003@gmail.com>
2026-02-17 16:54:12 +0900

    v1.1: 修正

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
   public static void main(String[] args) {
 <<<<<<< HEAD
-    System.out.println("SEMONSTER GAME");
-    System.out.println("Show a new monster!");
+    System.out.println("SEMonster!");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
 =======
     System.out.println("Hello SEMonster");
 >>>>>>> fa221b74ddb4d5e02c05e3225e5e1c0e808fd800
 
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show a new monster!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
   }
